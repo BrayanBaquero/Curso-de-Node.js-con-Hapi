3f1b807bb06cafc0f6a5180e4591d82811f5af96
Brayan Baquero <brayan.baquero.cz@gmail.com>
2022-03-23 08:08:42 -0500

    Logging con Good - Monitoreando el servidor

--- controllers/question.js
+++ controllers/question.js
@@ -25,10 +25,9 @@ async function createQuestion(req,h){
         }
        
        result=await question.create(req.payload,req.state.user,filename) 
-       console.log(`Pregunta creada con el ID${result}`)
+       req.log('info', `Pregunta creada con el ID${result}`)
     } catch (error) {
-        console.error(`Ocurrio un error ${error}`)
-
+        req.error('error', `Ocurrio un error ${error}`)
         return h.view('ask',{
             title: 'crear pregunta',
             error: 'Problemas creando la pregunta'
--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const vision = require('@hapi/vision')
 const routes=require('./routes')
 const site=require('./controllers/site')
 const methods=require('./lib/methods')
-
+const good=require('@hapi/good')
+const goodConsole=require('@hapi/good-console')
 
 
 const server = new Hapi.server({
@@ -25,6 +26,19 @@ async function init () {
   try {
     await server.register(inert)
     await server.register(vision)
+    await server.register({
+      plugin: good,
+      options: {
+        reporters:{
+          console:[
+            {
+              module:goodConsole
+            },
+            'stdout'
+          ]
+        }
+      }
+    })
 
     server.method('setAnswerRight',methods.setAnswerRight)
     server.method('getLast',methods.getLast,{
@@ -59,15 +73,15 @@ async function init () {
     process.exit(1)
   }
 
-  console.log(`Servidor iniciado en : ${server.info.uri}}`)
+  server.log('info',`Servidor iniciado en : ${server.info.uri}}`)
 }
 
 process.on('unhandledRejection', error=>{
-  console.error('UnhandledRejection', error.message,error);
+  server.log('UnhandledRejection',error)
 })
 
 process.on('unhandledException',error=>{
-  console.log('unhandledException',error.message,error);
+  server.log('unhandledException',error)
 })
 
 init()
